c06bdd3990b5db6a45c13f1155f86e8bc6eb0638
lining <lining201702@126.com>
2019-12-04 16:03:49 +0800

    uodate

--- test_plugin_demo/test/test_plugin_demo_test.dart
+++ test_plugin_demo/test/test_plugin_demo_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:test_plugin_demo/test_plugin_demo.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('test_plugin_demo');
